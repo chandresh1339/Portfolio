49aba3c4ae7270fc739e083d65aab939c6d64405
AnshKaushik <anshkaushik05@gmail.com>
2021-10-29 00:19:36 +0530

    fixed issue with navbar in mobile view

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ $(document).ready(function(){
     
     $('.menu-btn').click(function(){
         $('.navbar .max .menu').toggleClass("active"); 
-        for(var i=0;i<5;i++)
-        document.getElementsByClassName("navtoggle")[i].click(function(){
-            $('.navbar .menu').toggleClass("active");
-            $('.menu-btn i').toggleClass("active");
-    }
-    )
-    
+        $('.menu-btn i').toggleClass("active");
+        
+    })
 })
-})
+for(var i=0;i<5;i++)
+document.getElementsByClassName("navtoggle")[i].addEventListener("click",function(){
+    $('.navbar .menu').toggleClass("active");
+    $('.menu-btn i').toggleClass("active");
+}
+)
